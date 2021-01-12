ebbe2b6e72c1face2d7750ac0b80d90bd3e24960
yzt <609984807@qq.com>
2021-01-12 17:03:02 +0800

    ceshi

---
+++ src/Base/Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Captainbi\Base;
+
+class Test
+{
+    /**
+     * 构造方法
+     * @access public
+     * @param App $app 应用对象
+     */
+    public function __construct()
+    {
+        
+    }
+
+
+    protected function test()
+    {
+        return "hello";
+    }
+
+}
