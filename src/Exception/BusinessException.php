0ecd61551ffa63ff2b4de27ad29e8ee88f7fab6a
yzt <609984807@qq.com>
2021-01-12 16:05:27 +0800

    初始项目

---
+++ src/Exception/BusinessException.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types=1);
+
+namespace Captainbi\Exception;
+
+use Throwable;
+
+/**
+ * HTTP异常
+ */
+class BusinessException extends \RuntimeException
+{
+    public function __construct($code = 10001, $message = '', Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ src/Exception/Handle/BusinessExceptionHandle.php
@@ -0,0 +1,29 @@
+<?php
+declare (strict_types=1);
+
+namespace Captainbi\Exception\Handle;
+
+use Captainbi\Exception\BusinessException;
+use Captainbi\Util\Result;
+use think\exception\Handle;
+use think\exception\HttpException;
+use think\exception\ValidateException;
+use think\Response;
+use Throwable;
+
+/**
+ * HTTP异常
+ */
+class BusinessExceptionHandle extends Handle
+{
+    public function render($request, Throwable $e): Response
+    {
+        // 参数验证错误
+        if ($e instanceof BusinessException) {
+            return Result::fail($e->getCode(),$e->getMessage());
+        }
+        // 其他错误交给系统处理
+        return parent::render($request, $e);
+    }
+
+}
