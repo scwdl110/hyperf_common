9f1821c276c5671e4051176e3206da2e99c004c1
yzt <609984807@qq.com>
2021-01-14 15:47:15 +0800

    异常处理

--- src/Exception/Handle/BusinessExceptionHandle.php
+++ src/Exception/Handle/BusinessExceptionHandle.php
@@ -17,22 +17,17 @@ class BusinessExceptionHandle extends ExceptionHandler
 {
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
-//        // 判断被捕获到的异常是希望被捕获的异常
-//        if ($throwable instanceof BusinessException) {
-//            // 格式化输出
-//            $data = Result::fail([], $throwable->getCode(),$throwable->getMessage());
-//            // 阻止异常冒泡
-//            $this->stopPropagation();
-//            return $response->withStatus(500)->withBody(new SwooleStream($data));
-//        }
+        // 判断被捕获到的异常是希望被捕获的异常
+        if ($throwable instanceof BusinessException) {
+            // 格式化输出
+            $data = Result::fail([], $throwable->getCode(),$throwable->getMessage());
+            // 阻止异常冒泡
+            $this->stopPropagation();
+            return $response->withStatus(500)->withBody(new SwooleStream($data));
+        }
 
 //        // 交给下一个异常处理器
-//        return $response;
-        $data = Result::fail([], $throwable->getCode(),$throwable->getMessage());
-        // 阻止异常冒泡
-        $this->stopPropagation();
-        return $response->withStatus(500)->withBody(new SwooleStream($data));
-
+        return $response;
     }
 
     /**
