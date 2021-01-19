abe3905580961bf308211190cd2f1474e34741f0
yzt <609984807@qq.com>
2021-01-19 14:38:17 +0800

    1

--- src/Base/Model.php
+++ src/Base/Model.php
@@ -1,6 +1,6 @@
 <?php
 
-
+declare(strict_types=1);
 namespace Captainbi\Hyperf\Base;
 
 use Hyperf\DbConnection\Model\Model as BaseModel;
--- src/Base/Service.php
+++ src/Base/Service.php
@@ -1,11 +1,40 @@
 <?php
 
-
+declare(strict_types=1);
 namespace Captainbi\Hyperf\Base;
 
-
+use Captainbi\Hyperf\Util\Result;
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 
 class Service
 {
+    /**
+     * @Inject()
+     * @var ValidatorFactoryInterface
+     */
+    protected $validationFactory;
 
+    /**
+     * @param RequestInterface $request
+     * @param array $rule
+     * @param array $message
+     * @param array $attribute
+     * @return bool|string
+     */
+    protected function validate(RequestInterface $request, array $rule, array $message)
+    {
+        $validator = $this->validationFactory->make(
+            $request->all(),
+            $rule,
+            $message
+        );
+
+        if ($validator->fails()){
+            // Handle exception
+            return Result::fail([], $validator->errors()->first());
+        }
+
+        return Result::success();
+    }
 }
