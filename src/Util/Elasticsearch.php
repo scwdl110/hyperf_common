6586547b331f6ed0ae86a49090c9e0464b7c386a
yzt <609984807@qq.com>
2021-02-03 20:59:06 +0800

    es优化

--- src/Util/Elasticsearch.php
+++ src/Util/Elasticsearch.php
@@ -3,28 +3,14 @@ declare(strict_types=1);
 
 namespace Captainbi\Hyperf\Util;
 
-use Aws\Credentials\Credentials;
-use Aws\Signature\SignatureV4;
 use Elasticsearch\ClientBuilder;
 use Hyperf\Guzzle\RingPHP\CoroutineHandler;
 use Swoole\Coroutine;
 
 class Elasticsearch {
 
-    //凭据
-    protected $credentials = '';
-
-    //签名
-    protected $signature = '';
-
     //客户端
     protected $client = '';
-
-    public function __construct($key = '',$secret = '', $region = '') {
-        $this->credentials = new Credentials($key, $secret);
-        $this->signature = new SignatureV4('es', $region);
-
-    }
 
     /**
      * 返回客户端
@@ -33,7 +19,6 @@ class Elasticsearch {
      */
     final public function getClient(array $hosts = ['http://127.0.0.1:9200']){
         if(empty($this->client)){
-            $middleware = new \Wizacha\Middleware\AwsSignatureMiddleware($this->credentials, $this->signature);
             $clientBuilder =  ClientBuilder::create()->setConnectionPool('\Elasticsearch\ConnectionPool\SimpleConnectionPool', []);
             if (Coroutine::getCid() > 0) {
 //                $handler = make(PoolHandler::class, [
@@ -42,8 +27,7 @@ class Elasticsearch {
 //                    ],
 //                ]);
                 $handler = new CoroutineHandler();
-                $awsHandler = $middleware($handler);
-                $clientBuilder->setHandler($awsHandler);
+                $clientBuilder->setHandler($handler);
             }
             //            ->setHosts(['https://vpc-captain-search-test-yka42farqqrngrcags3vab6nhq.cn-northwest-1.es.amazonaws.com.cn']);
             //setRetries重试次数
