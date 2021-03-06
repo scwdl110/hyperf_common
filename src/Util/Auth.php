4ecbcc20ffa2c43ffa5ebd0637b03c3a04cd05c8
yzt <609984807@qq.com>
2021-03-06 18:34:58 +0800

    auth

--- src/Util/Auth.php
+++ src/Util/Auth.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Captainbi\Hyperf\Util;
 
 
+use Firebase\JWT\JWT;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Utils\ApplicationContext;
+
 class Auth {
 
     /**
@@ -29,6 +33,71 @@ class Auth {
             $data .= substr('====', $mod4);
         }
         return base64_decode($data);
+    }
+
+    /**
+     * @param array $payload
+     * @param string $key
+     * @param string $alg
+     * @return string
+     */
+    public static function jwtEncode(array $payload, string $key, string $alg = 'HS256'){
+        $jwt = JWT::encode($payload, $key, $alg);
+        $jwt = "Bearer ".$jwt;
+        return $jwt;
+    }
+
+    /**
+     * @param string $authorization
+     * @param string $keyName
+     * @return array|bool
+     */
+    public static function jwtDecode(string $authorization, string $keyName){
+        $data = [];
+        $jwt = trim(str_replace('Bearer', '', $authorization));
+        //权限验证
+        $authorizationArr = preg_split('/[\s\.]+/is', $authorization);
+        if(!isset($authorizationArr[1]) || !isset($authorizationArr[2])){
+            return false;
+        }
+
+        //获取key
+        $authorization = static::base64UrlDecode($authorizationArr[2]);
+        $authorizationJsonArr = json_decode($authorization,true);
+        if(!isset($authorizationJsonArr[$keyName])){
+            return false;
+        }
+        $data['key'] = $authorizationJsonArr[$keyName];
+        $data['ws_id'] = $authorizationJsonArr['ws_id'];
+
+        //获取加密方式
+        $authorization = Auth::base64UrlDecode($authorizationArr[1]);
+        $authorizationJsonArr = json_decode($authorization,true);
+        if(!isset($authorizationJsonArr['alg'])){
+            return false;
+        }
+        $data['alg'] = $authorizationJsonArr['alg'];
+
+        //kong配置
+        //后续优化加redis
+        $configInterface = ApplicationContext::getContainer()->get(ConfigInterface::class);
+        $kongConfig = $configInterface->get("pgsql.kong");
+        $kong = new Pgsql();
+        $client = $kong->getClient($kongConfig);
+        $client->prepare("my_query", "select consumer_id,secret from jwt_secrets where ws_id = $1 and key= $2");
+        $res = $client->execute("my_query", array($data['ws_id'], $data['key']));
+        if(!$res){
+            return false;
+        }
+        $jwtSecrets = $client->fetchAssoc($res);
+        if(!$jwtSecrets){
+            return false;
+        }
+
+        $data['consumer_id'] = $jwtSecrets['consumer_id'];
+        $data['secret'] = $jwtSecrets['secret'];
+
+        return $data;
     }
 
 }
