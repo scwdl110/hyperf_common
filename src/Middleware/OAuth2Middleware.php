5979378c5447c4f6eac7ee9fd02fea6b339aa6dd
consatan <consatan@gmail.com>
2021-04-07 10:35:14 +0800

    增加 oauth2 鉴权数据获取中间件

---
+++ src/Middleware/OAuth2Middleware.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace Captainbi\Hyperf\Middleware;
+
+use Hyperf\Utils\Context;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * 获取 OAuth 2.0 鉴权后的用户数据，包含
+ * admin_id (erp_base.b_user_admin.id)
+ * user_id (erp_base.b_user.id 既 erp_base.b_user_admin.user_id)
+ * is_master (erp_base.b_user_admin.is_master)
+ * dbhost (erp_base.b_user.dbhost)
+ * codeno (erp_base.b_user.codeno)
+ *
+ * 将保存在 ServerRequestInterface attribute 中，可通过
+ * $request->getAttribute('userInfo') 获取，获取到的是数组数据，数组结构如下
+ * [
+ *     'admin_id' => int,
+ *     'user_id' => int,
+ *     'is_master' => bool,
+ *     'dbhost' => string,
+ *     'codeno' => string,
+ * ]
+ */
+class OAuth2Middleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $authenticatedUserId = $request->getHeader('x-authenticated-userid');
+        $authenticatedUserId = $authenticatedUserId[0] ?? '';
+
+        if (1 !== preg_match('/^\d+:\d+:[01]:\d{3}:\d{3}$/', $authenticatedUserId)) {
+            return Context::get(ResponseInterface::class)->withStatus(401, 'Unauthorized');
+        }
+
+        $data = explode(':', $authenticatedUserId);
+        $request = $request->withAttribute('userInfo', [
+            'admin_id' => (int)$data[0],
+            'user_id' => (int)$data[1],
+            'is_master' => (bool)$data[2],
+            'dbhost' => $data[3],
+            'codeno' => $data[4],
+        ]);
+        Context::set(ServerRequestInterface::class, $request);
+
+        return $handler->handle($request);
+    }
+}
