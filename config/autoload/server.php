b041adbd87e1553f3d41b5a62c8568d098c4fbc1
yzt <609984807@qq.com>
2021-05-08 19:12:28 +0800

    houtai

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -40,7 +40,7 @@ return [
         'max_request' => 100000,
         'socket_buffer_size' => 2 * 1024 * 1024,
         'buffer_output_size' => 2 * 1024 * 1024,
-//        'daemonize' => 1
+        'daemonize' => 1
     ],
     'callbacks' => [
         Event::ON_WORKER_START => [Hyperf\Framework\Bootstrap\WorkerStartCallback::class, 'onWorkerStart'],
