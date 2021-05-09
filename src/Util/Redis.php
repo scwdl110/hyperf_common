6664286bf3c04d6514078397e4d12b9eca2c5ea0
yzt <609984807@qq.com>
2021-05-09 11:48:37 +0800

    redis

---
+++ src/Util/Redis.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Captainbi\Hyperf\Util;
+
+use Hyperf\Redis\RedisFactory;
+use Hyperf\Utils\ApplicationContext;
+
+class Redis {
+    //客户端
+    protected $client = '';
+
+    /**
+     * 返回redis客户端
+     * @param string $poolName
+     * @return \Hyperf\Redis\RedisProxy|string
+     */
+    final public function getClient(string $poolName  = 'default'){
+        if(empty($this->client)){
+            $container = ApplicationContext::getContainer();
+
+            // 通过 DI 容器获取或直接注入 RedisFactory 类
+            $this->client = $container->get(RedisFactory::class)->get($poolName);
+        }
+
+        return $this->client;
+    }
+
+}
