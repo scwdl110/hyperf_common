84339f496c69d1d67fce155cec855725c3bff480
Chopin Ngo <wushaobin@captainbi.com>
2021-05-19 15:44:38 +0800

    更新 exceptions.php

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 return [
     'handler' => [
         'http' => [
+            App\Exception\Handler\AppExceptionHandler::class,
 //            \Hyperf\ExceptionHandler\Handler\WhoopsExceptionHandler::class,
 //            Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
 //            App\Exception\Handler\AppExceptionHandler::class,
