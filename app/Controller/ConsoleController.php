480cb7b059150ba12f867de91fc3f2f32141e846
吴少彬 <wushaobin@captianbi.com>
2021-05-21 14:00:22 +0800

    测试修复 git pull 后依然提示版本差异问题

--- app/Controller/ConsoleController.php
+++ app/Controller/ConsoleController.php
@@ -41,7 +41,7 @@ class ConsoleController extends AbstractController
         $command = 'cd ' . BASE_PATH . '; git pull';
 
         if ($gitUrl) {
-            $command .= " {$gitUrl} `git rev-parse --abbrev-ref HEAD`";
+            $command .= " {$gitUrl} origin/`git rev-parse --abbrev-ref HEAD`";
         }
 
         $result = System::exec($command);
