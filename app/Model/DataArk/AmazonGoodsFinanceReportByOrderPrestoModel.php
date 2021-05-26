1973c623b7f849f42c3f609f819a86b7fc440857
wangdelin <scwdl110@126.com>
2021-05-26 16:52:12 +0800

    去掉表别人错误

--- app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
+++ app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
@@ -1035,7 +1035,7 @@ class AmazonGoodsFinanceReportByOrderPrestoModel extends AbstractPrestoModel
         }
 
         if(in_array('goods_views_rate', $targets) || in_array('goods_buyer_visit_rate', $targets)){
-            $table = "{$this->table_goods_day_report} AS report ";
+            $table = "{$this->table_goods_day_report} ";
             if($datas['min_ym'] == $datas['max_ym']){
                 $where  = "report.ym = '" . $datas['min_ym'] . "' AND  report.user_id_mod = " . ($datas['user_id'] % 20) ." AND " . $datas['origin_where'];
             }else{
@@ -1767,7 +1767,7 @@ class AmazonGoodsFinanceReportByOrderPrestoModel extends AbstractPrestoModel
         $time_fields = [];
 
         if($datas['time_target'] == 'goods_views_rate' || $datas['time_target'] == 'goods_buyer_visit_rate'){
-            $table = "{$this->table_goods_day_report} AS report ";
+            $table = "{$this->table_goods_day_report} ";
             if($datas['min_ym'] == $datas['max_ym']){
                 $where  = "report.ym = '" . $datas['min_ym'] . "' AND  report.user_id_mod = " . ($datas['user_id'] % 20) ." AND " . $datas['origin_where'];
             }else{
