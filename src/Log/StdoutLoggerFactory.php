b144951f972b54803834e090261134588911b82b
yzt <609984807@qq.com>
2021-05-31 18:55:31 +0800

    log

---
+++ src/Log/StdoutLoggerFactory.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Captainbi\Hyperf\Log;
+
+use Hyperf\Utils\ApplicationContext;
+use Psr\Container\ContainerInterface;
+
+class StdoutLoggerFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $loggerFactory = ApplicationContext::getContainer()->get("Hyperf\Logger\LoggerFactory");
+        return $loggerFactory->get('sys');
+    }
+}
