cc236d2ebc939d3e6138031415c095fc3b454ae7
yzt <609984807@qq.com>
2021-05-31 17:06:31 +0800

    stream

--- src/Stream/StreamOutput.php
+++ src/Stream/StreamOutput.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Console\Output;
+namespace Captainbi\Hyperf\Stream\StreamOutput;
 
 use Captainbi\Hyperf\Util\Log;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
