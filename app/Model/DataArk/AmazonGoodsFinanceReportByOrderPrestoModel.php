6b8c87ae9c2fdf0a6da09056e6185a565fc8a6ae
linzm <captain2020>
2021-06-09 16:02:17 +0800

    sql错误

--- app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
+++ app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
@@ -417,7 +417,7 @@ class AmazonGoodsFinanceReportByOrderPrestoModel extends AbstractPrestoModel
 
             if (!empty($where_detail['tag_id'])) {
                 if (strpos($group, 'tags_rel.tags_id') === false) {
-                    $table .= " LEFT JOIN {$this->table_amazon_goods_tags_rel} AS tags_rel ON tags_rel.goods_id = report.goods_g_amazon_goods_id AND tags_rel.db_num = '{$this->dbhost}' LEFT JOIN {$this->table_amazon_goods_tags} AS gtags ON gtags.id = tags_rel.tags_id AND gtags.db_num = '{$this->dbhost}";
+                    $table .= " LEFT JOIN {$this->table_amazon_goods_tags_rel} AS tags_rel ON tags_rel.goods_id = report.goods_g_amazon_goods_id AND tags_rel.db_num = '{$this->dbhost}' LEFT JOIN {$this->table_amazon_goods_tags} AS gtags ON gtags.id = tags_rel.tags_id AND gtags.db_num = '{$this->dbhost}'";
 
                 }
                 if(is_array($where_detail['tag_id'])){
