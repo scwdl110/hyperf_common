fc4811b0be014b9afbd1891ce9918bf4c10d7697
yzt <609984807@qq.com>
2021-06-17 17:19:10 +0800

    column

--- src/Util/Functions.php
+++ src/Util/Functions.php
@@ -6,12 +6,24 @@ namespace Captainbi\Hyperf\Util;
 
 class Functions {
 
-    public static function getUser(){
-
-    }
-
-    public static function getFile(){
-
+    /**
+     * 二维数组转化为某个字段为键值的数组
+     * @param array $array
+     * @param $column
+     * @return array|bool
+     */
+    public function arrayToFieldArray(array $array, $column)
+    {
+        if(!$array){
+            return false;
+        }
+        $newArray = [];
+        foreach ($array as $k=>$v){
+            if(isset($v[$column]) && isset($newArray[$v[$column]])){
+                $newArray[$v[$column]] = $v;
+            }
+        }
+        return $newArray;
     }
 
 }
