c5859c1395aade94e78fecd1ce5c5da8f5ead49f
qiujs <9jw45V1o8u>
2021-07-02 20:58:06 +0800

    测试同步恢复

--- app/Controller/DataArkController.php
+++ app/Controller/DataArkController.php
@@ -2,20 +2,14 @@
 
 namespace App\Controller;
 
-use Captainbi\Hyperf\Util\Logger;
 use Hyperf\Logger\LoggerFactory;
 use Hyperf\Utils\ApplicationContext;
 use Captainbi\Hyperf\Util\Result;
-use Psr\Log\LoggerInterface;
 
 class DataArkController extends AbstractController
 {
     protected function init($type = 1)
     {
-        $this->logger = new Logger();
-
-        $this->logger->execute_log(0,0,'test');
-        return [];
 
         $userInfo = $this->request->getAttribute('userInfo');
         $req = $this->request->all();
