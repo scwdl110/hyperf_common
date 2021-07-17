31f40f045434c5685fc011f1d9291d09b0a9fedf
yzt <609984807@qq.com>
2021-07-17 17:06:46 +0800

    跨域操作

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -15,13 +15,18 @@ class CorsMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $origin = $request->getHeader('origin') ?? '*';
+        $access_control = $request->getHeader('Access-Control-Request-Headers');
 
         $response = Context::get(ResponseInterface::class);
         $response = $response->withHeader('Access-Control-Allow-Origin', $origin)
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             // Headers 可以根据实际情况进行改写。
-//            ->withHeader('Access-Control-Allow-Headers', 'uid,token,Keep-Alive,User-Agent,Cache-Control,Content-Type,HYPERF_SESSION_ID_Token')
-            ->withHeader('Content-Type', 'application/json');
+//            ->withHeader('Access-Control-Allow-Headers', 'uid,token,Keep-Alive,User-Agent,Cache-Control,Content-Type,HYPERF_SESSION_ID_Token,X-Token,lang')
+            ->withHeader('Content-Type', 'application/json')
+            //该字段可选，用来指定本次预检请求的有效期，在此期间，不用发出另一条预检请求,20天。
+            ->withHeader('Access-Control-Max-Age', 1728000)
+            //根据浏览器提供的允许访问空值头设置
+            ->withHeader('Access-Control-Allow-Headers', $access_control);
 
         Context::set(ResponseInterface::class, $response);
 
