e221b055ccb3da90e434d942198519382742636b
json <you@example.com>
2021-07-21 11:55:24 +0800

    总计的页数只能为1恢复

--- app/Controller/DataArkController.php
+++ app/Controller/DataArkController.php
@@ -22,11 +22,12 @@ class DataArkController extends AbstractController
         $searchVal = trim(strval($req['searchVal'] ?? ''));
         $searchType = intval($req['searchType'] ?? 0);
         $params = $req['params'] ?? [];
-        if ($req['is_count'] == 1){//总计的页数只能为1
-            $page = 1;
-        }else{
-            $page = intval($req['page'] ?? 1);
-        }
+//        if ($req['is_count'] == 1){//总计的页数只能为1
+//            $page = 1;
+//        }else{
+//            $page = intval($req['page'] ?? 1);
+//        }
+        $page = intval($req['page'] ?? 1);
         $limit = intval($req['rows'] ?? 100);
         $sort = trim(strval($req['sort'] ?? ''));
         $order = trim(strval($req['order'] ?? ''));
