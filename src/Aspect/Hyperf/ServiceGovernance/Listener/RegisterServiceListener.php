bd4750678dfcfc2bde8a0f1e711db8c4aecd0013
DESKTOP-FL7306K\Administrator <751399695@qq.com>
2021-09-10 09:57:30 +0800

    AOP  Hyperf\ServiceGovernance\Listener\RegisterServiceListener::getServers  兼容docker  环境

--- src/Aspect/Hyperf/ServiceGovernance/Listener/RegisterServiceListener.php
+++ src/Aspect/Hyperf/ServiceGovernance/Listener/RegisterServiceListener.php
@@ -10,18 +10,21 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
-namespace Captainbi\Hyperf\Aspect\Hyperf\ServiceGovernance\Listener;
+namespace App\Aspect\Hyperf\ServiceGovernance\Listener;
 
+use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
 use Hyperf\Di\Aop\ProceedingJoinPoint;
 use Hyperf\ServiceGovernance\Listener\RegisterServiceListener as HyperfRegisterServiceListener;
-use Captainbi\Hyperf\Util\Functions;
 
+/**
+ * @Aspect
+ */
 class RegisterServiceListener extends AbstractAspect
 {
     // 要切入的类，可以多个，亦可通过 :: 标识到具体的某个方法，通过 * 可以模糊匹配
     public $classes = [
-        HyperfRegisterServiceListener::class . '::getServers',
+        HyperfRegisterServiceListener::class . '::getInternalIp',
     ];
 
     // 要切入的注解，具体切入的还是使用了这些注解的类，仅可切入类注解和类方法注解
@@ -30,35 +33,10 @@ class RegisterServiceListener extends AbstractAspect
 
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        $result = [];
-        $servers = config('server.servers', []);
-        foreach ($servers as $server) {
-            if (! isset($server['name'], $server['host'], $server['port'])) {
-                continue;
-            }
-            if (! $server['name']) {
-                throw new \InvalidArgumentException('Invalid server name');
-            }
+        //获取本服务的host
+        $host = config('servers.rpc_service_provider.local.host', null);
 
-            $host = $server['custom_host'] ?? $server['host']; // 使用自定义ip
-            if (in_array($host, ['0.0.0.0', 'localhost'])) {
-                $host = Functions::getInternalIp();
-            }
-
-            if (! filter_var($host, FILTER_VALIDATE_IP)) {
-                throw new \InvalidArgumentException(sprintf('Invalid host %s', $host));
-            }
-
-            $port = $server['custom_port'] ?? $server['port']; // 使用自定义port
-            if (! is_numeric($port) || ($port < 0 || $port > 65535)) {
-                throw new \InvalidArgumentException(sprintf('Invalid port %s', $port));
-            }
-            $port = (int) $port;
-            $result[$server['name']] = [$host, $port];
-        }
-
-        return $result;
+        return $host !== null ? $host : $proceedingJoinPoint->process();//本服务的host没有配置，就使用Hyperf框架本身提供的方法(Hyperf\ServiceGovernance\Listener\RegisterServiceListener::getInternalIp)获取
     }
-
 
 }
