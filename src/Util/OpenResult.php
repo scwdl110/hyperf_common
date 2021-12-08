32cfe5e940174f7851f09ebbe7c5147a75022f18
DESKTOP-UU243P5\rsoncai <234516038@qq.com>
2021-12-08 16:08:34 +0800

    添加开发平台返回格式更新

--- src/Util/OpenResult.php
+++ src/Util/OpenResult.php
@@ -20,17 +20,17 @@ class OpenResult
      * @param $code
      * @param $msg
      * @param array $data
-     * @param $nextToken
-     * @param $maxResult
+     * @param $next_token
+     * @param $max_result
      * @return string
      */
-    private static function data($code, $msg, $data = [], $nextToken = '', $maxResult = 0)
+    private static function data($code, $msg, $data = [], $next_token = '', $max_result = 0)
     {
         return json_encode(array(
             'code' => $code ?: 0,
             'msg' => $msg ?: 'success',
-            'nextToken' => $nextToken ?: '',
-            'maxResult' => $maxResult ?: 0,
+            'next_token' => $next_token ?: '',
+            'max_result' => $max_result ?: 0,
             'data' => (object)$data,
         ), JSON_UNESCAPED_UNICODE);
     }
@@ -39,13 +39,13 @@ class OpenResult
      * 成功
      * @param array $data
      * @param string $msg
-     * @param string $nextToken
-     * @param int $maxResult
+     * @param string $next_token
+     * @param int $max_result
      * @return mixed
      */
-    public static function success($data = [], $nextToken = '', $maxResult = 0, $msg = 'success')
+    public static function success($data = [], $next_token = '', $max_result = 0, $msg = 'success')
     {
-        return self::data(200, $msg, $data, $nextToken, $maxResult);
+        return self::data(200, $msg, $data, $next_token, $max_result);
     }
 
     /**
