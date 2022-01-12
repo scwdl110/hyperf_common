121e47d02d292985ac8fa11f3ddaa9ecd06443fe
linzm <captain2020>
2022-01-12 14:59:07 +0800

    erp里

--- app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
+++ app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
@@ -14548,6 +14548,7 @@ COALESCE(goods.goods_operation_pattern ,2) AS goods_operation_pattern
                 $rt_sql = "SELECT {$rt_field} FROM new_origin_table " ;
                 $rt_join = !empty($fba_data['join']) ? $fba_data['join'] : "" ;
                 $rt_where = !empty($fba_data['where']) ? $fba_data['where'] : "" ;
+                $count_table_group = empty($fba_data['group']) ? '' : " GROUP BY {$fba_data['group']}";
 
                 if(!empty($fba_data['child_table'])){
                     foreach($fba_data['child_table'] as $c=>$cdata){
@@ -14556,7 +14557,7 @@ COALESCE(goods.goods_operation_pattern ,2) AS goods_operation_pattern
                                 $newTables[] = "{$cdata['table_name']}  AS (select fabTmp.* from (SELECT report.channel_id  FROM {$table} {$where} group by report.channel_id) AS FBAOriginTabel LEFT JOIN ({$cdata['table_sql']} ) AS fabTmp ON fabTmp.channel_id = FBAOriginTabel.channel_id AND fabTmp.channel_id is NOT NULL )  " ;
                             }elseif($fba_data['dimension'] == 'sku'){
                                 $newTables[] = " {$cdata['table_name']} AS ( {$cdata['table_sql']} ) "  ;
-                                $newTables[] = "count_table AS (SELECT max(report.user_id) AS user_id,max(amazon_goods.goods_sku) AS sku,max(report.channel_id) AS channel_id FROM {$table} WHERE {$where} GROUP BY {$group})";
+                                $newTables[] = "count_table AS (SELECT max(report.user_id) AS user_id,max(amazon_goods.goods_sku) AS sku,max(report.channel_id) AS channel_id FROM {$table} WHERE {$where} {$count_table_group})";
                             }else{
                                 $newTables[] = " {$cdata['table_name']} AS ( {$cdata['table_sql']} ) "  ;
                             }
