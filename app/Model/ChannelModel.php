ddcd72b82b28e30e4babc399497bc5496148019a
json <you@example.com>
2022-02-15 13:51:27 +0800

    【店铺添加权限信息】

--- app/Model/ChannelModel.php
+++ app/Model/ChannelModel.php
@@ -11,10 +11,39 @@ declare(strict_types=1);
  */
 namespace App\Model;
 
+use App\Lib\Redis;
 use Captainbi\Hyperf\Base\Model;
+use Captainbi\Hyperf\Util\Unique;
 
 class ChannelModel extends Model
 {
     protected $connection = 'erp_base';
     protected $table = 'channel';
+
+
+
+    const CHANNEL_OPERATION_IDS_REDIS_KEY='jdx_channel_operation_ids_';
+
+    private $redis;
+
+
+    public function getChannelOperationPatternList($user_id)
+    {
+        $this->redis = new Redis();
+        $redis_key = self::CHANNEL_OPERATION_IDS_REDIS_KEY . $user_id;
+        $channel_operation_data = $this->redis->get($redis_key);
+        if (false===$channel_operation_data) {
+            $where = array(
+                ['user_id', '=', $user_id],
+                ['goods_operation_pattern', '=', 2],
+                ['operation_user_admin_id', '>', 0],
+                ['status',  '=', 1]
+            );
+            $channel_operation_data = Unique::getArray(UserAdminModel::where($where)->get(["id","operation_user_admin_id"]));
+            $this->redis->set($redis_key, $channel_operation_data,4*60*60);
+        }
+        $channel_operation_data = !empty($channel_operation_data) ? $channel_operation_data : [];
+        return $channel_operation_data;
+    }
+
 }
