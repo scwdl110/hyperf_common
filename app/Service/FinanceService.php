f0a75c4857b2cd8669608a017703233e21586e2c
json <you@example.com>
2022-02-17 19:44:37 +0800

    【isku权限过滤】

--- app/Service/FinanceService.php
+++ app/Service/FinanceService.php
@@ -165,6 +165,7 @@ class FinanceService extends BaseService
             }
         }
         $params['priv_goods_operation_user_admin_id'] = "";
+        $params['priv_goods_isku_id'] = "";
         //商品和运营人员 添加商品权限控制
         if ($type > 0 && isset($req['priv_key'])){
             $goods_priv=$this->getUserGoodsPriv($req['priv_key'],$userInfo);
@@ -176,17 +177,33 @@ class FinanceService extends BaseService
                         $related_user_admin_ids_str = $goods_priv['related_user_admin_ids_str'];
                         if(!empty($related_user_admin_ids_str))
                         {
+                            //查出关联的负责人和开发人员
                             $sql = "select id from e_amazon_goods_isku_user_{$userInfo['codeno']} where user_id = {$userInfo['user_id']} and link_admin_ids regexp ',(".str_replace(",","|",$related_user_admin_ids_str)."),'";
-                            $isku_user = Db::connection("bigdata_goods_ads_{$userInfo['dbhost']}")->select($sql);
+                            $isku_user = Db::connection("erp_captain_{$userInfo['dbhost']}")->select($sql);
                             if (!empty($isku_user)){
                                 foreach ($isku_user as $key => $value){
                                     $isku_user[$key] = (array) $value;
                                 }
+                                $isku_user_str = implode(",",array_column($isku_user,'id'));
+                                //查出关联的isku
+                                $sql = "select id from e_amazon_goods_isku_{$userInfo['codeno']} where user_id = {$userInfo['user_id']} and (head_id in ({$isku_user_str}) or developer_id in({$isku_user_str}) or ispu_id in (select id from e_amazon_goods_ispu_{$userInfo['codeno']} where user_id ={$userInfo['user_id']} and (head_id in ({$isku_user_str}) or developer_id in ({$isku_user_str})) and is_delete=0)) and status >0";
+                                $isku_arr = Db::connection("erp_captain_{$userInfo['dbhost']}")->select($sql);
+                                if (!empty($isku_arr)) {
+                                    foreach ($isku_arr as $key => $value) {
+                                        $isku_arr[$key] = (array)$value;
+                                    }
+                                    $params['priv_goods_isku_id'] = $isku_arr;
+                                    $where .= "  AND report.goods_isku_id IN (" . implode(",",array_column($isku_arr,'id')) . ")";
+                                }else{
+                                    return $result;
+                                }
+
                             }else{
-                                $isku_user = array();
+                                return $result;
                             }
-                            $params['priv_goods_operation_user_admin_id'] = $related_user_admin_ids_str;
-                            $where .= "  AND report.goods_operation_user_admin_id IN (" . $related_user_admin_ids_str . ")";
+
+
+
                         }else{
                             return $result;
                         }
