969e5df9806760df67bf2178bc00911eafeef317
yjm <290393077@qq.com>
2022-03-04 13:59:43 +0800

    22902 【3.2】【ark】【1.8.1】【报表详情-维度下钻】子账号下钻运营人员（按商品），一个运营人员出现多条数据

--- app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
+++ app/Model/DataArk/AmazonGoodsFinanceReportByOrderPrestoModel.php
@@ -14291,6 +14291,9 @@ COALESCE(goods.goods_operation_pattern ,2) AS goods_operation_pattern
                 $where .= " AND dc.user_department_id IN({$datas['where_parent']['child_dimension_department_id']}) AND dc.status = 1";
                 $child_dimension_sql = " LEFT JOIN {$this->table_department_channel} as dc ON dc.channel_id = c.id and dc.user_id = c.user_id ";
             }
+            if (!empty($datas['where_parent']['child_dimension_site_id'])){
+                $where .= " AND c.site_id IN({$datas['where_parent']['child_dimension_site_id']})";
+            }
             if($is_currency_exchange == 1 || !empty(array_intersect(['fba_goods_value','fba_total_ltsf','fba_ltsf_6_12','fba_ltsf_12','fba_estimate_total'],$this->lastTargets ))){
                 if (empty($currencyInfo) || $currencyInfo['currency_type'] == '1') {
                     $table_sql = "select {$fba_field1} from {$this->table_amazon_fba_inventory_tend_v3} as tend LEFT JOIN (select c_tmp.id ,c_tmp.user_id , c_tmp.site_id , c_tmp.merchant_id , area.area_id, c_tmp.operation_user_admin_id from  {$this->table_channel} as c_tmp LEFT JOIN {$this->table_area} as area ON area.site_id = c_tmp.site_id where c_tmp.user_id = {$datas['user_id']} AND c_tmp.operation_user_admin_id > 0 AND c_tmp.goods_operation_pattern = 2) as c ON tend.user_id = c.user_id AND tend.merchant_id  = c.merchant_id AND tend.area_id = c.area_id {$child_dimension_sql} LEFT JOIN  {$this->table_site_rate} as rates ON rates.site_id = c.site_id AND rates.user_id = 0".$where." group by c.site_id,c.operation_user_admin_id" ;
